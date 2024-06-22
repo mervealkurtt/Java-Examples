a6dfa1e7c836f81f9b2781e29c6acb5368bb7669
Merve Alkurt <mrvalkrtt97@gmail.com>
2024-06-22 16:17:43 -0500

    first version

---
+++ src/Day01_variables_scanner/C01_PrimitiveDataTypes.java
@@ -0,0 +1,33 @@
+package Day01_variables_scanner;
+
+public class C01_PrimitiveDataTypes {
+
+    public static void main(String[] args) {
+
+        char chr1 = 'o';
+        char chr2 = ' ';
+        //char chr3 = 'a ';     // Too many characters in character literal
+
+        byte byt = 10;      //8 bit
+        short sh = 10;      //16 bit
+        int sayi = 10;      //32 bit
+        long lng = 10;      //64 bit
+
+        int sayi1 = 0;      //32 bit
+        int sayi2 = 1000;      //32 bit
+        int sayi3 = 1000000;      //32 bit
+        int sayi4 = 1000000000;      //32 bit
+
+        float flt = 2312345789234230f;
+        float flt2 = 234F;
+        float flt3 = 23.68f;
+        double dbl = 3.14287956654789333434234;
+
+        /*
+            1- boolean -> true/false
+            2- char -> one character, use ''
+            3,4,5,6- byte,short,int,long -> integer number
+            7,8- float,double -> decimal number
+         */
+    }
+}
